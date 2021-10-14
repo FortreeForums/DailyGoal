d5f3e26b13affc91d45e6ad0d7c513302e5f32b0
Fortree Forums <rennofthemill@hotmail.co.uk>
2021-10-14 14:24:07 +0100

    Create xf_ap_daily_goals_history table

--- Setup.php
+++ Setup.php
@@ -37,4 +37,27 @@ class Setup extends AbstractSetup
 	use StepRunnerInstallTrait;
 	use StepRunnerUpgradeTrait;
 	use StepRunnerUninstallTrait;
+	
+	public function upgrade1060070Step1()
+	{
+		$this->installStep1();
+	}
+	
+	public function installStep1()
+	{
+		$this->schemaManager()->createTable('xf_ap_daily_goal_history', 
+		function(Create $table)
+    		{
+        		$table->addColumn('date', 'int');
+        		$table->addColumn('stats_type', 'varchar', 30);
+        		$table->addColumn('counter', 'int')->nullable(true)->setDefault(null);
+        		$table->addColumn('fulfilled', 'bool');
+        		$table->addPrimaryKey('date');
+    		});
+	}
+	
+	public function uninstallStep1()
+	{
+		$this->schemaManager()->dropTable('xf_ap_daily_goal_history');
+	}
 }
