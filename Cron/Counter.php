97047648fccaa8a3058bc53a50392bb3faacf99c
Fortree Forums <rennofthemill@hotmail.co.uk>
2021-10-16 20:13:44 +0100

    Auto-adjust goals if conditions are met

--- Cron/Counter.php
+++ Cron/Counter.php
@@ -32,7 +32,7 @@ class Counter
 		$db = \XF::db();
 		$app = \XF::app();
 		$options = \XF::options();
-		$forums = $options->ap_dg_post_excluded_nodes;
+		$forums = $options->apDgExcludedNodesPosts;
 		$forum_id = implode(",", $forums);
 
 		if(empty($forum_id))
@@ -40,9 +40,9 @@ class Counter
 			$forum_id = '0';
 		}
 
-		if(!$options->ap_dg_disable_post_goal)
+		if(!$options->apDgDisablePostGoal)
 		{
-			$post = $db->fetchOne('SELECT count(p.post_id) AS count 
+			$postCount = $db->fetchOne('SELECT count(p.post_id) AS count 
 					FROM xf_post AS p
                     			LEFT JOIN xf_thread AS t ON (t.thread_id = p.thread_id)
                     			LEFT JOIN xf_forum AS f ON (f.node_id = t.node_id)
@@ -54,24 +54,24 @@ class Counter
                     	
                     	if(array_key_exists('UW/FCS', $addons) 
 			&& $addons['UW/FCS'] >= 1
-			&& $options->ap_dg_include_comments)
+			&& $options->apDgIncludeComments)
 			{                    				
-                    		$comment = $db->fetchOne('SELECT COUNT(c.comment_id) AS count
+                    		$commentCount = $db->fetchOne('SELECT COUNT(c.comment_id) AS count
                     			FROM xf_uw_comment AS c
                     			LEFT JOIN xf_thread AS t ON (t.thread_id = c.thread_id)
                     			LEFT JOIN xf_forum AS f ON (f.node_id = t.node_id)
                     			WHERE DATE(FROM_UNIXTIME(c.comment_date)) = CURDATE()
                     			AND f.node_id NOT IN (?)', [$forum_id]);
                     				  
-                    		$cache = ($post + $comment);
+                    		$count = ($postCount + $commentCount);
                     	}
                     	else
                     	{
-                    		$cache = $post;
+                    		$count = $postCount;
                     	}
                     	
 			$simpleCache = $app->simpleCache();
-			$simpleCache['apathy/DailyGoal']['count'] = $cache;
+			$simpleCache['apathy/DailyGoal']['count'] = $count;
 		}
 	}
 
@@ -80,7 +80,7 @@ class Counter
 		$db = \XF::db();
 		$app = \XF::app();
 		$options = \XF::options();
-		$forums = $options->ap_dg_thread_excluded_nodes;
+		$forums = $options->apDgExcludedNodesThreads;
 		$forum_id = implode(",", $forums);
 
 		if(empty($forum_id))
@@ -88,15 +88,15 @@ class Counter
 			$forum_id = '0';
 		}
 
-		if(!$options->ap_dg_disable_thread_goal)
+		if(!$options->apDgDisableThreadGoal)
 		{
-			$cache = $db->fetchOne('SELECT count(thread_id) AS threadCount
+			$count = $db->fetchOne('SELECT count(thread_id) AS threadCount
 					FROM xf_thread
 					WHERE DATE(FROM_UNIXTIME(post_date)) = CURDATE()
                     			AND node_id NOT IN (?)', [$forum_id]);
 
 			$simpleCache = $app->simpleCache();
-			$simpleCache['apathy/DailyGoal']['threadCount'] = $cache;
+			$simpleCache['apathy/DailyGoal']['threadCount'] = $count;
 		}
 	}
 
@@ -106,14 +106,14 @@ class Counter
 		$app = \XF::app();
 		$options = \XF::options();
 
-		if(!$options->ap_dg_disable_member_goal)
+		if(!$options->apDgDisableMemberGoal)
 		{
-			$cache = $db->fetchOne('SELECT count(user_id) AS memberCount
+			$count = $db->fetchOne('SELECT count(user_id) AS memberCount
 					FROM xf_user
 					WHERE DATE(FROM_UNIXTIME(register_date)) = CURDATE()');
 
 			$simpleCache = $app->simpleCache();
-			$simpleCache['apathy/DailyGoal']['memberCount'] = $cache;
+			$simpleCache['apathy/DailyGoal']['memberCount'] = $count;
 		}
 	}
 
@@ -125,12 +125,12 @@ class Counter
 
 		$simpleCache = $app->simpleCache();
 
-		if(!$options->ap_disable_post_goal)
+		if(!$options->apDgDisablePostGoal)
 		{
-			// Submit the total to xf_stats_daily
+			// Submit the total to xf_ap_daily_goal_history
 			$total = $simpleCache['apathy/DailyGoal']['count'];
 			
-			if($total >= $options->ap_dg_post_goal)
+			if($total >= $options->apDgPostGoal)
 			{
 				$fulfilled = 1;
 			}
@@ -144,13 +144,48 @@ class Counter
 				    [\XF::$time, 'post_goal', $total, $fulfilled]);
 				    
 			$simpleCache['apathy/DailyGoal']['count'] = 0;
+			
+			// Auto-adjust goal if needed
+			$timeframe = $options->apDgAutoAdjustTimeframePosts;
+			$weight = $options->apDgAutoAdjustWeightPosts;
+			
+			$entity = \XF::finder('apathy\DailyGoal:History')->fetch();
+			
+			$streak = 0;
+			
+			foreach($entity as $goal)
+			{
+				if($goal['stats_type'] == 'post_goal' && $goal['fulfilled'] == 1)
+				{
+					$streak++;
+				}
+				
+				if($goal['stats_type'] == 'post_goal' && $goal['fulfilled'] == 0)
+				{
+					$streak = 0;
+				}
+			}
+			
+			$option = \XF::em()->find('XF:Option', 'apDgPostGoal');
+				
+			if($streak >= $timeframe)
+			{
+				$option->option_value = ( $options->apDgPostGoal + $weight );
+			}
+			elseif($streak <= $timeframe)
+			{
+				$option->option_value = ( $options->apDgPostGoal - $weight );
+			}
+			
+			$option->save();
 		}
-		if(!$options->ap_disable_thread_goal)
+		
+		if(!$options->apDgDisableThreadGoal)
 		{
-			// Submit the total to xf_stats_daily
+			// Submit the total to xf_ap_daily_goal_history
 			$total = $simpleCache['apathy/DailyGoal']['threadCount'];
 			
-			if($total >= $options->ap_dg_thread_goal)
+			if($total >= $options->apDgThreadGoal)
 			{
 				$fulfilled = 1;
 			}
@@ -164,13 +199,48 @@ class Counter
 				    [\XF::$time, 'thread_goal', $total, $fulfilled]);
 				    
 			$simpleCache['apathy/DailyGoal']['threadCount'] = 0;
+			
+			// Auto-adjust goal if needed
+			$timeframe = $options->apDgAutoAdjustTimeframeThreads;
+			$weight = $options->apDgAutoAdjustWeightThreads;
+			
+			$entity = \XF::finder('apathy\DailyGoal:History')->fetch();
+			
+			$streak = 0;
+			
+			foreach($entity as $goal)
+			{
+				if($goal['stats_type'] == 'thread_goal' && $goal['fulfilled'] == 1)
+				{
+					$streak++;
+				}
+				
+				if($goal['stats_type'] == 'thread_goal' && $goal['fulfilled'] == 0)
+				{
+					$streak = 0;
+				}
+			}
+			
+			$option = \XF::em()->find('XF:Option', 'apDgThreadGoal');
+				
+			if($streak >= $timeframe)
+			{
+				$option->option_value = ( $options->apDgThreadGoal + $weight );
+			}
+			elseif($streak <= $timeframe)
+			{
+				$option->option_value = ( $options->apDgThreadGoal - $weight );
+			}
+			
+			$option->save();
 		}
-		if(!$options->ap_disable_member_goal)
+		
+		if(!$options->apDgDisableMemberGoal)
 		{			
-			// Submit the total to xf_stats_daily
+			// Submit the total to xf_ap_daily_goal_history
 			$total = $simpleCache['apathy/DailyGoal']['memberCount'];
 			
-			if($total >= $options->ap_dg_member_goal)
+			if($total >= $options->apDgMemberGoal)
 			{
 				$fulfilled = 1;
 			}
@@ -184,6 +254,40 @@ class Counter
 				    [\XF::$time, 'member_goal', $total, $fulfilled]);
 				    
 			$simpleCache['apathy/DailyGoal']['memberCount'] = 0;
+			
+			// Auto-adjust goal if needed
+			$timeframe = $options->apDgAutoAdjustTimeframeMembers;
+			$weight = $options->apDgAutoAdjustWeightMembers;
+			
+			$entity = \XF::finder('apathy\DailyGoal:History')->fetch();
+			
+			$streak = 0;
+			
+			foreach($entity as $goal)
+			{
+				if($goal['stats_type'] == 'member_goal' && $goal['fulfilled'] == 1)
+				{
+					$streak++;
+				}
+				
+				if($goal['stats_type'] == 'member_goal' && $goal['fulfilled'] == 0)
+				{
+					$streak = 0;
+				}
+			}
+			
+			$option = \XF::em()->find('XF:Option', 'apDgMemberGoal');
+				
+			if($streak >= $timeframe)
+			{
+				$option->option_value = ( $options->apDgMemberGoal + $weight );
+			}
+			elseif($streak <= $timeframe)
+			{
+				$option->option_value = ( $options->apDgMemberGoal - $weight );
+			}
+			
+			$option->save();
 		}
 	}
 }
